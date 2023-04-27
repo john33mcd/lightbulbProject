459675ca928b72f1c7d3b7df28eceac2f727514e
john33mcd <jmcd-34@hotmail.com>
2023-04-27 20:39:32 +0000

    developed post and comment models

--- lightbulbApp/admin.py
+++ lightbulbApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 # Registers model with admin panel.
 admin.site.register(Post)
+admin.site.register(Comment)
--- lightbulbApp/models.py
+++ lightbulbApp/models.py
@@ -2,18 +2,41 @@ from django.db import models
 # inbuilt user model pre made by django
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Model created for each post to blog
 
 class Post(models.Model):
     # cascade deletes post if user is removed or deleted
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    slug = models.SlugField(default="", max_length=200, unique=True, null=False)
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
     # sets default title, sets default ordering 
 
     def __str_(self):
         return self.title
 
+    def number_of_likes(self):
+        return self.likes.count()
+
     class Meta:
         ordering = ['created']
+
+# Model created for comments on posts
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
