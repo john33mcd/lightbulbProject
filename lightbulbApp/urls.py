929bb1c06041efac0ffa23958d69cf7371885f6f
john33mcd <jmcd-34@hotmail.com>
2023-05-01 16:25:40 +0000

    fixed errors wherein URLs were not linking correctly

--- lightbulbApp/urls.py
+++ lightbulbApp/urls.py
@@ -4,9 +4,9 @@ from .views import MyLoginView, CreateTask, Comments, PostList
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('', PostList.as_view(), name='index'),
     path('login/', MyLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('create-task/', CreateTask.as_view(), name='create-task'),
-    path('<slug:slug>/', views.Comments.as_view(), name='comments'),
-    path('', views.PostList.as_view(), name='index'),
+    path('<slug:slug>/', Comments.as_view(), name='comments'),
 ]
--- lightbulbApp/views.py
+++ lightbulbApp/views.py
@@ -2,35 +2,36 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 from django.urls import reverse_lazy
-
+from django.views.generic.list import ListView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.views.generic.edit import CreateView
 
 
 # fields are already created in loginview template
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('created')
+    template_name = 'index.html'
+    context_object_name = 'post-list'
+    paginate_by = 6
+
+
 class MyLoginView(LoginView):
     template_name = 'login.html'
     fields = '__all__'
     redirect_authenticated_user = True
 
     def get_success_url(self):
-        return reverse_lazy('PostList')
-
-
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.order_by('created')
-    template_name = 'index.html'
-    context_object_name = 'post_list'
-    paginate_by = 6
+        return reverse_lazy('index')
 
 
 class CreateTask(CreateView):
     model = Post
     fields = ['title', 'author', 'description']
     template_name = 'createTask.html'
-    success_url = reverse_lazy('posts')
+    success_url = reverse_lazy('index')
 
 
 class Comments(View):
