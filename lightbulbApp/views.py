a23a3937c0589240e6ee5681f99dbd4181fa83a3
john33mcd <jmcd-34@hotmail.com>
2023-05-02 12:21:49 +0000

    tested registration

--- lightbulbApp/views.py
+++ lightbulbApp/views.py
@@ -39,6 +39,9 @@ class PostDetail(View):
             },
         )
 
+# views and functionality codeblocks for login and registration
+# taken from youtube tutorial, more in readMe
+
 
 class MyLoginView(LoginView):
     template_name = 'login.html'
@@ -55,7 +58,11 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('index')
 
-    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
 
 
 class CreateTask(CreateView):
