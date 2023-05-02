816d64648eb67f28e782128e0a6ac578e00c9494
john33mcd <jmcd-34@hotmail.com>
2023-05-02 12:26:32 +0000

    linked up logout functionality correctly

--- lightbulbApp/urls.py
+++ lightbulbApp/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', PostList.as_view(), name='index'),
     path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='registration'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('create-task/', CreateTask.as_view(), name='create-task'),
 ]
