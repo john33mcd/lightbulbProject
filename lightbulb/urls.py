4f231f532e33556abc594de082bd4cea3d1ca2da
john33mcd <jmcd-34@hotmail.com>
2023-05-02 16:47:43 +0000

    positioned responsive login and logout

--- lightbulb/urls.py
+++ lightbulb/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lightbulbApp.urls'), name="lightbulb-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
